459755c20c5c5316c1665a96f2bf555c187f0f9e
pablo <26plau26@gmail.com>
2020-12-06 00:45:09 -0500

    feat: "Good" error handling at ./auzoom/__init__.py
    This was done making a custom error at ./auzoom/__init__.py, now when
    the platform is not MacOs, the error message looks like this:
    
    Traceback (most recent call last):
      File "example_error.py", line 1, in <module>
        import auzoom
      File "/Users/notmyname/code/auzoom/auzoom/__init__.py", line 16, in <module>
        raise NotMacosError("auzoom only works on MACOS\nIf you have any suggestions open an issue or a pull request at:\nhttps://www.github.com/baplou/auzoom")
    auzoom.NotMacosError: auzoom only works on MACOS
    If you have any suggestions open an issue or a pull request at:
    https://www.github.com/baplou/auzoom

--- auzoom/__init__.py
+++ auzoom/__init__.py
@@ -1,11 +1,16 @@
 import os
 import platform
 
-if os.name == "posix" and platform.system() == "Darwin":
+if os.name == "fdskjposix" and platform.system() == "Darwin":
   # only works on macos
   from auzoom.main import ZoomClient
 else:
-  print("auzoom only works on MACOS")
-  print("Support for WINDOWS 10 will be added later")
-  print("if you have any suggestions open an issue or a pull request at:")
-  quit()
+  class NotMacosError(Exception):
+    pass
+
+  #print("auzoom only works on MACOS")
+  #print("Support for WINDOWS 10 will be added later")
+  #print("If you have any suggestions open an issue or a pull request at:")
+  #print("https://www.github.com/baplou/auzoom")
+
+  raise NotMacosError("auzoom only works on MACOS\nIf you have any suggestions open an issue or a pull request at:\nhttps://www.github.com/baplou/auzoom")
